8b8d56005fc11c205870a010e31d2a568ae8759a
Matteo <mat.cardilli@stud.uniroma3.it>
2022-06-01 16:45:54 +0200

    aggiunto oauth login with google and facebook

--- catering/src/main/java/spring/catering/controller/AuthenticationController.java
+++ catering/src/main/java/spring/catering/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -40,16 +41,23 @@ public class AuthenticationController {
 	
 	//vai alla pagin index (o admin/dashboard) dopo il login
 	@GetMapping("/default")
-    public String defaultAfterLogin(Model model) {
-        UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Credentials credentials = cs.getCredentials(userDetails.getUsername());
-        
-        if(credentials.getRole().equals(Credentials.ADMIN_ROLE)) {
-            return "admin/dashboard.html";
-        }
-        
-        return "index.html";
-    }
+	public String defaultAfterLogin(Model model) {
+		UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Credentials credentials = cs.getCredentials(userDetails.getUsername());
+		
+		if(credentials.getRole().equals(Credentials.ADMIN_ROLE)) {
+			return "admin/dashboard.html";
+		}
+
+		return "index.html";
+	}
+	
+	//vai alla pagin index (o admin/dashboard) dopo il login con OAuth ***DA MODELLARE BENE***
+	@GetMapping("/defaultOauth")
+	public String oauthLogin(Model model) {
+		OAuth2User userDetails = (OAuth2User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		return "index.html";
+	}
 	
 	//vai alla pgina di registrazione di un utente
 	@GetMapping("/register")
